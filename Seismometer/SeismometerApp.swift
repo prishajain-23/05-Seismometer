90bfcd2a1dcc317349589e9ab3055379ceaaf3a7
John Henry Thompson <jht1493@gmail.com>
2023-02-20 00:13:39 -0500

    init

---
+++ Seismometer/SeismometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeismometerApp.swift
+//  Seismometer
+//
+//  Created by jht2 on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SeismometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SeismometerBrowser()
+        }
+    }
+}
